81fd146ef1b4d167110449680902e9fa0fd7bb49
zhouzhenwei <529779677@qq.com>
2019-07-02 20:36:55 +0800

    活动接口及实现类

--- src/main/java/com/coding/sales/service/impl/Discounts.java
+++ src/main/java/com/coding/sales/service/impl/Discounts.java
@@ -1,16 +1,28 @@
 package com.coding.sales.service.impl;
 
-import com.coding.sales.input.OrderCommand;
-import com.coding.sales.service.PreciousMetalActivities;
 
+import com.coding.datamodel.PreciousMetal;
+import com.coding.sales.service.PreciousMetalActivities;
 import java.math.BigDecimal;
+
 
 public class Discounts implements PreciousMetalActivities {
     @Override
-    public BigDecimal activities(OrderCommand order) {
-        BigDecimal totalAmt = null;
-
-        return  totalAmt;
+    public BigDecimal activities(PreciousMetal metal,BigDecimal amt) {
+            BigDecimal lessamt = null;
+            String discountcard = metal.getDiscountCard();
+            BigDecimal price = metal.getPrice();
+            BigDecimal discoutnine = new BigDecimal(0.1);
+            BigDecimal discoutninefive = new BigDecimal(0.05);
+            if(discountcard!=null && !discountcard.equals("")){
+                if("九折券".equals(discountcard)){
+                    lessamt=   amt.multiply(price).multiply(discoutnine);
+                }
+                if("九五折券".equals(discountcard)){
+                    lessamt =  amt.multiply(price).multiply(discoutninefive);
+                }
+            }
+            return  lessamt;
 
     }
 }
