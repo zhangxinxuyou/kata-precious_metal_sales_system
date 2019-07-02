170c327b25063763d62dd0d032930b08df8552f7
zhangxinxuyou <zhangxinxuyou@163.com>
2019-07-02 21:23:04 +0800

    折扣类提交

---
+++ src/main/java/com/coding/constant/DiscountConstant.java
@@ -0,0 +1,6 @@
+package com.coding.constant;
+
+public class DiscountConstant {
+	public static final String  NINEFIVE_DISCOUNT = "95折券";
+	public static final String  NINE_DISCOUNT = "9折券";
+}
--- src/main/java/com/coding/datamodel/DiscountCard.java
+++ src/main/java/com/coding/datamodel/DiscountCard.java
@@ -1,7 +1,64 @@
 package com.coding.datamodel;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class DiscountCard {
 	
+	
+     private String DiscountCardName;
+     
+     private BigDecimal discount;
+    
+    
+     public BigDecimal getProductAmount(PreciousMetal preciousMetal,BigDecimal productAmount,DiscountCard discountCard) {
+    	BigDecimal returnAccount = new BigDecimal("0");
+ 		
+ 		BigDecimal beforeaccount = new BigDecimal("0");
+ 		
+ 		BigDecimal preciousMetalPrice = preciousMetal.getPrice();
+ 		
+ 		beforeaccount = beforeaccount.add(productAmount.multiply(preciousMetalPrice));
+ 		
+ 		List<DiscountCard> discountCardList = preciousMetal.getDiscountCardList();
+ 		
+ 		if(discountCardList==null||discountCardList.size()<1) {
+ 			return returnAccount;
+ 		}
+ 		for(int i=0;i<discountCardList.size();i++) {
+ 			
+ 			DiscountCard proDiscountCard=discountCardList.get(i);
+ 			if(proDiscountCard.getDiscountCardName().equals(discountCard.getDiscountCardName())) {
+ 				
+ 				returnAccount = beforeaccount.multiply(discountCard.getDiscount());
+ 			}
+ 		}
+ 		
+ 		
+ 		
+ 		return returnAccount;
+ 		
+     }
+
+
+	public String getDiscountCardName() {
+		return DiscountCardName;
+	}
+
+
+	public void setDiscountCardName(String discountCardName) {
+		DiscountCardName = discountCardName;
+	}
+
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+
+	public void setDiscount(BigDecimal discount) {
+		this.discount = discount;
+	}
 	
 
 }
