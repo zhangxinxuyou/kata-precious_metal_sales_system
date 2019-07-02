e79f122199d8df88805803ff85648a349017ebd1
zhangxinxuyou <zhangxinxuyou@163.com>
2019-07-02 21:24:50 +0800

    删除类

--- src/main/java/com/coding/sales/service/PreciousMetalActivities.java
+++
@@ -1,9 +0,0 @@
-package com.coding.sales.service;
-
-import com.coding.sales.input.OrderCommand;
-
-import java.math.BigDecimal;
-
-public interface PreciousMetalActivities {
-    BigDecimal activities(OrderCommand order);
-}
