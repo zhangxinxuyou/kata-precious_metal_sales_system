5e69ef6751069a24e4522df1771383fdc81cd8f6
zhangxinxuyou <zhangxinxuyou@163.com>
2019-07-02 18:07:49 +0800

    商品信息初始化

---
+++ src/main/java/com/coding/constant/ProductInfo.java
@@ -0,0 +1,12 @@
+package com.coding.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductInfo {
+	public static final Map map = new HashMap();
+	static {
+		
+	};
+
+}
